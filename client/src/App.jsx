44788b97fab4ed87dfa753c91980416a52d7d833
ZygimantasL <129725364+ZygimantasL@users.noreply.github.com>
2023-09-19 09:47:47 +0300

    Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ContextWrapper } from "./context/GlobalContext";
+import { PublicLayout } from './layout/PublicLayout';
+import { Page404 } from './pages/Page404';
+import { Home } from './pages/Home';
 
 function App() {
-  return (<>
-  </>  
+  return (
+    <ContextWrapper>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={PublicLayout}>
+            <Route index path='/' element={<Home />}></Route>
+          <Route Component={PublicLayout}>
+            <Route path='*' element={<Page404 />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ContextWrapper>
   );
 }
 
